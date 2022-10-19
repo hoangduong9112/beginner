7b352de4bd819a7db4cc585caa46a23826ed0717
tienifr <christianwen18@gmail.com>
2022-10-20 01:11:45 +0700

    fix: add info module into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { InfoModule } from 'src/info/info.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [InfoModule],
   controllers: [AppController],
   providers: [AppService],
 })
